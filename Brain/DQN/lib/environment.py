67e7f49169843f59d705803f8b4c122cf8ce73a5
Lewis <a046829713@gmail.com>
2025-03-06 11:27:59 +0800

    udpate

--- Brain/DQN/lib/environment.py
+++ Brain/DQN/lib/environment.py
@@ -23,7 +23,7 @@ class State:
         self.N_steps = 1000  # 這遊戲目前使用多少步學習
         self.model_train = model_train
         self.default_slippage = default_slippage
-        self.win_payoff_weight = 1
+        self.win_payoff_weight = 0.8
         self.build_fileds(init_prices)
 
     def build_fileds(self, init_prices):
